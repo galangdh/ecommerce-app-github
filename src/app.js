a52cfb8a82334671175d58e095de53d92dd493c1
SatriyaWibawa <satriyarock88@gmail.com>
2025-08-02 14:41:56 +0800

    feat:create products new

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
-const productRoutes = require('./routes/productRoutes');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+app.use(express.json()); // Middleware untuk parsing JSON body
+const productRoutes = require("./routes/productRoutes");
 
-app.use('/api/products', productRoutes);
+// Rute untuk setiap modul
+app.use("/api/transactions", productRoutes);
+// Rute dasar
+app.get("/", (req, res) => {
+  res.send("Welcome to the E-commerce API!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server running on port ${PORT}');
+  console.log('Access API at http://localhost:${PORT}/api');
+});
